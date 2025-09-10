1d3aeed302251456f300b862db8e64140b51e5af
ikennaex <ikennaexcel@gmail.com>
2025-09-10 09:27:48 +0100

    navigate - protected routes

--- src/Contexts/ProtectedRoutes.jsx
+++ src/Contexts/ProtectedRoutes.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
 import { UserContext } from "./UserContext";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import Loader from "../Components/Loaders/Loader";
 
 const ProtectedRoutes = () => {
     const { user, loading } = useContext(UserContext);
+    const navigate = useNavigate()
 
     if (loading) {
         // Optionally show a loader while checking auth
@@ -12,7 +13,7 @@ const ProtectedRoutes = () => {
     }
 
     // Redirect to login if user is not authenticated
-    return user ? <Outlet /> : <Navigate to="/login" />;
+    return user ? <Outlet /> : navigate("/login");
 };
 
 export default ProtectedRoutes;
