05c7d85f29cf9b962f9015b40ccf091c0e6e47ed
ikennaex <ikennaexcel@gmail.com>
2025-09-13 11:32:29 +0100

    loading state

--- src/Components/Escorts/Escorts.jsx
+++ src/Components/Escorts/Escorts.jsx
@@ -6,58 +6,14 @@ import {
 } from "@heroicons/react/24/solid";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router";
-import { baseUrl } from "../../baseUrl";
 import { fetchEscorts } from "./fetchEscorts";
+import Loader from "../Loaders/Loader";
 
-// const escorts = [
-//   {
-//     id: 1,
-//     name: "Stella",
-//     image:
-//       "https://img.freepik.com/premium-photo/portrait-smiling-young-woman-red-dress-footpath_1048944-29109377.jpg?ga=GA1.1.2145612538.1736353082&semt=ais_hybrid&w=740&q=80",
-//     phone: "+234 705 333 3484",
-//     location: "Ikeja, Lagos",
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ullam expedita ducimus animi fugit facilis sit repellat hic nemo ipsa?",
-//   },
-
-//   {
-//     id: 2,
-//     name: "Mercy",
-//     image:
-//       "https://img.freepik.com/premium-photo/studio-photoshoot-modeling_1048944-3927801.jpg?ga=GA1.1.2145612538.1736353082&semt=ais_hybrid&w=740&q=80",
-//     phone: "+234 705 333 3484",
-//     location: "Magodo, Lagos",
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ullam expedita ducimus animi fugit facilis sit repellat hic nemo ipsa?",
-//   },
-
-//   {
-//     id: 3,
-//     name: "Jane",
-//     image:
-//       "https://img.freepik.com/free-photo/woman-wearing-red-body-suit-red-background_633478-239.jpg?ga=GA1.1.2145612538.1736353082&semt=ais_hybrid&w=740&q=80",
-//     phone: "+234 705 333 3484",
-//     location: "Yaba, Lagos",
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ullam expedita ducimus animi fugit facilis sit repellat hic nemo ipsa?",
-//   },
-
-//   {
-//     id: 4,
-//     name: "Stella",
-//     image:
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2apztSw2a9Nzw3jedprGNLItfXNrf2jI_cw&s",
-//     phone: "+234 705 333 3484",
-//     location: "Ikeja, Lagos",
-//     description:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ullam expedita ducimus animi fugit facilis sit repellat hic nemo ipsa?",
-//   },
-// ];
 
 const Escorts = () => {
+  const [loading, setLoading] = useState(true)
   const [escorts, setEscorts] = useState([]);
-  const [error, setError] = useState("")
+  const [error, setError] = useState("");
   // for pagination
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
@@ -65,25 +21,28 @@ const Escorts = () => {
   const loadEscorts = async () => {
     try {
       const response = await fetchEscorts(page, 10);
-      if (response) {
-        console.log(response)
-        setEscorts(response.escortDoc);
-        setTotalPages(response.totalPages);
-      }
+      console.log(response);
+      setEscorts(response.escortDoc);
+      setTotalPages(response.totalPages);
+      setLoading(false)
     } catch (err) {
-      setError(err.respone.data.message);
+      setLoading(false)
+      setError(err.message);
       console.log(err);
     }
   };
 
+  console.log(error);
+
   useEffect(() => {
-    loadEscorts()
-  }, [page])
+    loadEscorts();
+  }, [page]);
 
   return (
     <div className="p-1 bg-black py-10">
+        {loading && <div className="flex items-center justify-center"><Loader /></div>}
       <div className="grid lg:grid-cols-4 grid-cols-2 gap-3">
-        {error && <div className="text-red-700">{error}</div>}
+        {error && <div className="text-red-700 text-center flex items-center justify-center">{error}</div>}
         {escorts.map((item) => (
           <Link to={`/escorts/${item._id}`}>
             <div
@@ -122,7 +81,9 @@ const Escorts = () => {
                 {/* Location */}
                 <div className="flex items-center gap-2 lg:mb-2">
                   <MapPinIcon className="h-4 text-white hidden md:block" />
-                  <p className="lg:text-sm text-[10px]">{item.city + ", " + item.state + ", " +  item.country}</p>
+                  <p className="lg:text-sm text-[10px]">
+                    {item.city + ", " + item.state + ", " + item.country}
+                  </p>
                 </div>
 
                 {/* Description */}
--- src/Components/Escorts/fetchEscorts.js
+++ src/Components/Escorts/fetchEscorts.js
@@ -2,21 +2,13 @@ import axios from "axios";
 import { baseUrl } from "../../baseUrl";
 
 export const fetchEscorts = async (page = 1, limit = 10, filters = {}) => {
-  try {
-    // build query string
-    const queryParams = new URLSearchParams({
-      page,
-      limit,
-      ...filters, // e.g. { location: "Lagos", category: "VIP" }
-    }).toString();
+  // build query string
+  const queryParams = new URLSearchParams({
+    page,
+    limit,
+    ...filters,
+  }).toString();
 
-    // make request
-    const res = await axios.get(`${baseUrl}escorts?${queryParams}`);
-
-    // response contains escorts + pagination info
-    return res.data;
-  } catch (err) {
-    console.error("Error fetching escorts:", err.response?.data || err.message);
-    return null;
-  }
+  const res = await axios.get(`${baseUrl}escorts?${queryParams}`);
+  return res.data; // if error happens, it will throw and be caught in loadEscorts
 };
