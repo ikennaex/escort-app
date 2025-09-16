50c4237a0c2c1577b830782ab10161543906c3a8
Fortune <fortuneokechukwu206@gmail.com>
2025-09-16 10:15:19 +0100

    getbank details error

--- src/Pages/EscortBankDetailsPage/EscortBankDetailsPage.jsx
+++ src/Pages/EscortBankDetailsPage/EscortBankDetailsPage.jsx
@@ -1,9 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../../Contexts/UserContext";
+import { baseUrl } from "../../baseUrl";
+import { useNavigate } from "react-router";
+import Loader from "../../Components/Loaders/Loader";
 
 const EscortBankDetailsPage = () => {
   const { user } = useContext(UserContext);
-  console.log(user);
+  const navigate = useNavigate();
+  const { api } = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
+  const [bankDetails, setBankDetails] = useState({
+    bankName: "",
+    accountName: "",
+    accountNumber: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBankDetails((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  console.log(bankDetails);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const response = await api.patch(
+        `${baseUrl}escorts/bankdetails`,
+        bankDetails
+      );
+      console.log(response);
+      alert(response.data.message);
+      console.log(bankDetails);
+      navigate(`/escortdashboard/${user._id}`);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-pink-50">
@@ -12,14 +51,16 @@ const EscortBankDetailsPage = () => {
           Bank Details
         </h2>
 
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <div>
             <label className="block text-sm font-medium text-gray-600 mb-1">
               Account Number
             </label>
             <input
               type="number"
+              name="accountNumber"
               placeholder="Enter account number"
+              onChange={handleChange}
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-400"
             />
           </div>
@@ -30,7 +71,9 @@ const EscortBankDetailsPage = () => {
             </label>
             <input
               type="text"
+              name='bankName'
               placeholder="Enter bank name"
+              onChange={handleChange}
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-400"
             />
           </div>
@@ -41,16 +84,19 @@ const EscortBankDetailsPage = () => {
             </label>
             <input
               type="text"
+              name='accountName'
               placeholder="Enter account name"
+              onChange={handleChange}
               className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-400"
             />
           </div>
 
           <button
+          disabled ={loading}
             type="submit"
-            className="w-full bg-pink-500 text-white font-semibold py-2 rounded-lg hover:bg-pink-600 transition"
+            className="w-full bg-pink-500 text-white font-semibold py-2 rounded-lg hover:bg-pink-600 transition disabled:bg-customPink/20 mx-auto flex justify-center items-center"
           >
-            Save Bank Details
+            {loading? <Loader/> : "Save Bank Details" }
           </button>
         </form>
       </div>
--- src/Pages/EscortDashboardPage/EscortDashboard.jsx
+++ src/Pages/EscortDashboardPage/EscortDashboard.jsx
@@ -1,5 +1,15 @@
-import React, { useState } from "react";
-import { Phone, Mail, Eye, Share, Menu, Newspaper, Play, BookImage, SquarePlay, User,
+import React, { useState, useEffect } from "react";
+import {
+  Phone,
+  Mail,
+  Eye,
+  Share,
+  Menu,
+  Newspaper,
+  Play,
+  BookImage,
+  SquarePlay,
+  User,
   Pencil,
   Calendar,
   Hand,
@@ -19,20 +29,42 @@ import { Phone, Mail, Eye, Share, Menu, Newspaper, Play, BookImage, SquarePlay, 
   Lock,
   Power,
   Trash2,
-  Activity,  } from 'lucide-react';
+  Activity,
+} from "lucide-react";
 import { Link } from "react-router";
 import { useContext } from "react";
-import {UserContext} from '../../Contexts/UserContext'
+import { UserContext } from "../../Contexts/UserContext";
+import { baseUrl } from "../../baseUrl";
+import { useNavigate } from "react-router";
 
 const EscortDashboard = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState("Menu"); // default tab
-  const {user} = useContext(UserContext)
-  console.log(user)
+  const { user } = useContext(UserContext);
+  const [bankDetails, setBankDetails] = useState()
+  const { api } = useContext(UserContext);
+  console.log(user);
+
+  const getBankDetails = async () => {
+    try {
+      const response = await api.get(
+        `${baseUrl}escorts/bankdetails`);
+      
+      setBankDetails(response.data);
+      console.log(response.data);
+      navigate(`/escortdashboard/${user._id}`);
+    } catch (err) {
+      console.log(err);
+    } 
+  };
+
+  useEffect(() => {
+    getBankDetails();
+  }, []);
 
   return (
     <div className="lg:flex h-screen bg-pink-200 text-white gap-5 justify-center">
-
       <div className="lg:w-[38%] lg:mt-4 justify-center">
         <div className="rounded-xl bg-pink-100 p-4 flex flex-col gap-4 text-black">
           <div className="flex justify-between items-start">
@@ -86,7 +118,6 @@ const EscortDashboard = () => {
         </div>
 
         <div className="mt-5 rounded-xl bg-pink-100 p-4 flex flex-col gap-6 text-black">
-
           {/* BOOST Section */}
           <div className="flex justify-between items-center border-b pb-2">
             <p className="font-bold">BOOST</p>
@@ -105,7 +136,9 @@ const EscortDashboard = () => {
             <div className="flex-1 rounded-xl bg-pink-500 text-white p-4 flex flex-col justify-between">
               <div>
                 <p className="font-bold">Boost My Profile</p>
-                <p className="text-sm text-pink-100">More chances to match with extra features.</p>
+                <p className="text-sm text-pink-100">
+                  More chances to match with extra features.
+                </p>
               </div>
               <button className="mt-4 border-2 border-yellow-400 text-yellow-400 font-bold rounded-full py-1 px-4">
                 BOOST NOW - 100
@@ -116,7 +149,9 @@ const EscortDashboard = () => {
             <div className="flex-1 rounded-xl bg-green-500 text-white p-4 flex flex-col justify-between">
               <div>
                 <p className="font-bold">Make Available Today</p>
-                <p className="text-sm text-green-100">More chances to match with extra features.</p>
+                <p className="text-sm text-green-100">
+                  More chances to match with extra features.
+                </p>
               </div>
               <button className="mt-4 border-2 border-yellow-400 text-yellow-400 font-bold rounded-full py-1 px-4">
                 AVAILABLE NOW - 100
@@ -126,16 +161,17 @@ const EscortDashboard = () => {
 
           {/* Footer Section */}
           <div className="flex justify-between items-center text-sm">
-            <p className="text-pink-500">Last login at <span className="font-bold">10:50 AM · Mon</span></p>
-            <p className="text-gray-600">Registered on <span className="font-bold">Sep 15, 2025</span></p>
+            <p className="text-pink-500">
+              Last login at <span className="font-bold">10:50 AM · Mon</span>
+            </p>
+            <p className="text-gray-600">
+              Registered on <span className="font-bold">Sep 15, 2025</span>
+            </p>
           </div>
-
         </div>
-
       </div>
 
       <div className="lg:w-[58%] mt-4 rounded-xl bg-pink-100">
-
         <nav className="mx-3 rounded-lg bg-[#fff8f9] my-4 py-4 px-1">
           <ul className="flex gap-6 whitespace-nowrap px-4 text-gray-700">
             <div
@@ -151,7 +187,7 @@ const EscortDashboard = () => {
                 Menu
               </li>
             </div>
-  
+
             <div
               onClick={() => setActiveTab("Timeline")}
               className="flex gap-1 items-center cursor-pointer"
@@ -166,7 +202,7 @@ const EscortDashboard = () => {
               </li>
               ·<p>0</p>
             </div>
-  
+
             <div
               onClick={() => setActiveTab("Story")}
               className="flex gap-1 items-center cursor-pointer"
@@ -181,7 +217,7 @@ const EscortDashboard = () => {
               </li>
               ·<p>0</p>
             </div>
-  
+
             <div
               onClick={() => setActiveTab("Gallery")}
               className="flex gap-1 items-center cursor-pointer"
@@ -196,7 +232,7 @@ const EscortDashboard = () => {
               </li>
               ·<p>0</p>
             </div>
-  
+
             <div
               onClick={() => setActiveTab("Videos")}
               className="flex gap-1 items-center cursor-pointer"
@@ -213,7 +249,7 @@ const EscortDashboard = () => {
             </div>
           </ul>
         </nav>
-  
+
         {/* CONTENT */}
         <div className="bg-[#fff8f9] my-4 mx-3 p-4 text-black">
           {activeTab === "Menu" && (
@@ -223,85 +259,93 @@ const EscortDashboard = () => {
                 {/* Item */}
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/${user._id}`}>
-                  <User className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">My Profile</p></Link>
+                    <User className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">My Profile</p>
+                  </Link>
                 </div>
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/edit/${user._id}`}>
-                  <Pencil className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">Edit Profile</p></Link>
+                    <Pencil className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">Edit Profile</p>
+                  </Link>
                 </div>
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/booking/${user._id}`}>
-                  <Calendar className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">My Bookings</p></Link>
+                    <Calendar className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">My Bookings</p>
+                  </Link>
                 </div>
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/requests/${user._id}`}>
-                  <Hand className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">My Requests</p></Link>
+                    <Hand className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">My Requests</p>
+                  </Link>
                 </div>
-
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/wallet/${user._id}`}>
-                  <Wallet className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">My Wallet</p></Link>
+                    <Wallet className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">My Wallet</p>
+                  </Link>
                 </div>
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/gifts/${user._id}`}>
-                  <Gift className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">My Gifts</p></Link>
+                    <Gift className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">My Gifts</p>
+                  </Link>
                 </div>
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/boost-profile/${user._id}`}>
-                  <Rocket className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">Boost Profile</p></Link>
+                    <Rocket className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">Boost Profile</p>
+                  </Link>
                 </div>
-
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/blacklist/${user._id}`}>
-                  <Ban className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">Blacklisted Client</p></Link>
+                    <Ban className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">Blacklisted Client</p>
+                  </Link>
                 </div>
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/bank/${user._id}`}>
-                  <Landmark className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">Bank Details</p></Link>
+                    <Landmark className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">Bank Details</p>
+                  </Link>
                 </div>
-
-
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/events/${user._id}`}>
-                  <CalendarDays className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">My Events</p></Link>
+                    <CalendarDays className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">My Events</p>
+                  </Link>
                 </div>
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/reports/${user._id}`}>
-                  <FileWarning className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">Pending Reports</p></Link>
+                    <FileWarning className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">Pending Reports</p>
+                  </Link>
                 </div>
-
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/change-password/${user._id}`}>
-                  <Lock className="h-8 w-8 text-pink-500" />
-                  <p className="mt-2 text-sm">Change Password</p></Link>
+                    <Lock className="h-8 w-8 text-pink-500" />
+                    <p className="mt-2 text-sm">Change Password</p>
+                  </Link>
                 </div>
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
                   <Link to={`/escorts/status/${user._id}`}>
-                  <Activity className="h-8 w-8 text-green-500" />
-                  <p className="mt-2 text-sm">Status</p></Link>
+                    <Activity className="h-8 w-8 text-green-500" />
+                    <p className="mt-2 text-sm">Status</p>
+                  </Link>
                 </div>
 
                 <div className="flex flex-col items-center justify-center bg-white shadow-sm rounded-lg p-4 cursor-pointer hover:bg-pink-50">
@@ -318,7 +362,6 @@ const EscortDashboard = () => {
           )}
         </div>
       </div>
-      
     </div>
   );
 };
