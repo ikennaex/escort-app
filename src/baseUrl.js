508ce3462d2c7efba65dc297f3ec28b7c46f540b
ikennaex <ikennaexcel@gmail.com>
2025-11-09 07:54:22 +0100

    update: vite

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,8 +1,5 @@
 const isDev = false; // set to true for local development
+const localhost = import.meta.env.VITE_LOCAL_HOST;
+const live = import.meta.env.VITE_LIVE_HOST;
 
-export const baseUrl = isDev
-  ? "http://localhost:4000/"
-  : "https://escort-app-backend.vercel.app/";
-
-
-  // https://escort-app-backend-wapy.onrender.com/
+export const baseUrl = isDev ? localhost : live;
