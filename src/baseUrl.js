b88d45ec5eb262fe169016b0f897a08d0e425202
ikennaex <ikennaexcel@gmail.com>
2026-01-07 17:18:26 +0100

    update: liveTest

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,5 +1,7 @@
 const isDev = true; // set to true for local development
 const localhost = import.meta.env.VITE_LOCAL_HOST;
 const live = import.meta.env.VITE_LIVE_HOST;
+const liveTest = import.meta.env.VITE_LIVE_TEST_HOST;
 
-export const baseUrl = isDev ? localhost : live;
+// export const baseUrl = isDev ? localhost : live;
+export const baseUrl = isDev ? localhost : liveTest;
